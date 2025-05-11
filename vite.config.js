8c42e420ada743e3d25fa6cf933e0928b34ec8f0
Robert Waszkowski <waszkowski@tecna.pl>
2025-05-11 20:27:01 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
+// import { defineConfig } from "vite";
+// import path from "path";
+// import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+// export default defineConfig({
+//   plugins: [cssInjectedByJsPlugin()],
+
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, "./lib/notations/BpmnDiagram.js"),
+//       name: "Aurea EDEN",
+//       fileName: (format) => `bpmn-diagram.${format}.js`,
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import path from "path";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
@@ -8,8 +24,24 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "./lib/notations/BpmnDiagram.js"),
-      name: "Aurea EDEN",
+      name: "AureaEDEN",
       fileName: (format) => `bpmn-diagram.${format}.js`,
+      formats: ['es', 'umd']
     },
-  },
+    rollupOptions: {
+      external: ['three'],
+      output: {
+        globals: {
+          three: 'THREE'
+        }
+      }
+    },
+    sourcemap: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: false
+      }
+    }
+  }
 });
